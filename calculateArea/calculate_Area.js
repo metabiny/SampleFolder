ded3b9b46732433d19bce4494e3ee7ff356d60e8
Biniam <metabiny@outlook.com>
2025-03-20 13:37:06 -0400

    HTML & JS files added

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,10 +1,23 @@
 let length;
 let width;
 
+let amount1;
+let amount2;
+let amount3;
+
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
 
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
+
+function totalAmount() {
+    amount1 = parseFloat(document.getElementById('grocery1').value);
+    amount2 = parseFloat(document.getElementById('grocery2').value);
+    amount3 = parseFloat(document.getElementById('grocery3').value);
+
+    let total = amount1 + amount2 + amount3;
+    document.getElementById('total').innerText = `The total amount is: ${total}`;
 }
---
+++ followersCount/followers_count.js
@@ -0,0 +1,20 @@
+let count = 0;
+
+
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML = count;
+}
+
+function increaseCount() {
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function checkCountValue() {
+    if (count === 10) {
+        alert("Your Instagram post gained 10 followers! Congratulation!");
+    } else if (count === 20) {
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
