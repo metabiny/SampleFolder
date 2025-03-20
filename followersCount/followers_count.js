43db64a59518cc55394d8a5732536b0488950cfa
Biniam <metabiny@outlook.com>
2025-03-20 14:14:18 -0400

    Files added

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -18,3 +18,8 @@ function checkCountValue() {
         alert("Your Instagram post gained 20 followers! Keep it up!");
     }
 }
+
+function resetCount() {
+    count = 0;
+    document.getElementById('countDisplay').innerHTML = count;
+}
